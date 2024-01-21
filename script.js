80d19825da60fe3f842f015a1d45f2c738af54d3
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2024-01-21 22:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//complete this code
 class Person {
 	constructor(name,age){
 		this.name = name;
 		this.age = age;
 	}
-	
+
 	get Name(){
 		return this.name;
 	}
@@ -15,17 +14,23 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is stduying`)
+		console.log(`${this.name} is studying`)
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is teaching
-		`)
+		console.log(`${this.name} is teaching`)
 	}
 }
+
 let obj = new Person('John', 25);
+
+console.log(obj.Name); // Outputs: John
+
+obj.Age = 30; // Sets the age to 30
+
+console.log(obj.age); // Outputs: 30
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
