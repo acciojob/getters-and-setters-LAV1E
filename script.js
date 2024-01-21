eda458d16e7af790326c72a748436db7c61f92d6
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2024-01-21 21:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(){
+		this.name = name;
+		this.age = age;
+	}
+	get Name(){
+		return this.name;
+	}
+	set age(){
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is stduying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching
+		`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
